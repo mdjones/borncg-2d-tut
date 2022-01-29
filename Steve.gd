d73f00062bcafef27140b1c515a820518c5e839e
Michael D. Jones <michaeld.jones@novartis.com>
2022-01-29 16:37:59 -0500

    Part 18 (Running & Finite State Machines) https://www.youtube.com/watch?v=HMvnMmwJ0sk

--- Steve.gd
+++ Steve.gd
@@ -1,38 +1,70 @@
 extends KinematicBody2D
 
+enum States {AIR = 1, FLOOR, LADDER, WALL}
+var state = States.AIR
+
 var velocity = Vector2(0,0)
 
-const SPEED = 180
-const GRAVITY = 50
-const JUMP_FORCE = -2300
+const SPEED = 210
+const RUNSPEED = 400
+const GRAVITY = 35
+const JUMP_FORCE = -1100
 
 func _physics_process(delta):
-	if Input.is_action_pressed("right"):
-		velocity.x = SPEED
-		$Sprite.play("walk")
-		$Sprite.flip_h = false
-	elif Input.is_action_pressed("left"):
-		velocity.x = -SPEED
-		$Sprite.play("walk")
-		$Sprite.flip_h = true
-	else:
-		$Sprite.play("idle")
-		
-	if not is_on_floor():
-		$Sprite.play("air")
-	
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = JUMP_FORCE
-			$SoundJump.play()
+	match state:
+		States.AIR:
+			if is_on_floor():
+				state = States.FLOOR
+				continue
+			$Sprite.play("air")
+			if Input.is_action_pressed("right"):
+				velocity.x = lerp(velocity.x, SPEED, 0.1) if velocity.x < SPEED else lerp(velocity.x, SPEED, 0.03)
+				$Sprite.flip_h = false
+			elif Input.is_action_pressed("left"):
+				velocity.x = lerp(velocity.x, -SPEED, 0.1) if velocity.x < -SPEED else lerp(velocity.x, -SPEED, 0.03)
+				$Sprite.flip_h = true
+			else:
+				velocity.x = lerp(velocity.x, 0, 0.05)
+			move_and_fall()
+		States.FLOOR:
+			if not is_on_floor():
+				state = States.AIR
+				continue
+			if Input.is_action_pressed("right"):
+				if Input.is_action_pressed("run"):
+					velocity.x = lerp(velocity.x,RUNSPEED,0.1)
+					$Sprite.set_speed_scale(1.8)
+				else:
+					velocity.x = lerp(velocity.x,SPEED,0.1)
+					$Sprite.set_speed_scale(1.0)
+				$Sprite.play("walk")
+				$Sprite.flip_h = false
+			elif Input.is_action_pressed("left"):
+				if Input.is_action_pressed("run"):
+					velocity.x = lerp(velocity.x,-RUNSPEED,0.1)
+					$Sprite.set_speed_scale(1.8)
+				else:
+					velocity.x = lerp(velocity.x,-SPEED,0.1)
+					$Sprite.set_speed_scale(1.0)
+				$Sprite.play("walk")
+				$Sprite.flip_h = true
+			else:
+				$Sprite.play("idle")
+				velocity.x = lerp(velocity.x, 0, 0.05)
+			if Input.is_action_just_pressed("jump"):
+				velocity.y = JUMP_FORCE
+				$SoundJump.play()
+				state = States.AIR
+			move_and_fall()
 
+
+
+	
+	#velocity.y = lerp(velocity.y, 0, 0.05)
+
+func move_and_fall():
 	velocity.y = velocity.y + GRAVITY
-
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
-	velocity.x = lerp(velocity.x, 0, 0.05)
-	velocity.y = lerp(velocity.y, 0, 0.05)
-
 
 func _on_FallZone_body_entered(body):
 	get_tree().change_scene("res://GameOver.tscn")
